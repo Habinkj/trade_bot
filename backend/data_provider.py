d33d08bce7c9c8049646b92d7f93f8895cb6bb21
Muhammed Fardan Naheem <fardu8351@gmail.com>
2026-02-05 15:25:09 +0530

    incresing speed

--- backend/data_provider.py
+++ backend/data_provider.py
@@ -1,15 +1,38 @@
+import time
 import pandas as pd
 from datetime import datetime, timedelta
 from backend.zerodha_session import get_kite
 from backend.instruments import get_token
 
-def get_historical(symbol, interval="5minute", days=5):
-    kite = get_kite()   # ✅ INSIDE function (only runs after login)
-    token = get_token(symbol)
+CACHE = {}
+CACHE_EXPIRY = 300  # 5 minutes
+
+
+def get_historical(symbol):
+    now = time.time()
+
+    # Return cached data if available
+    if symbol in CACHE:
+        df, timestamp = CACHE[symbol]
+        if now - timestamp < CACHE_EXPIRY:
+            return df
+
+    kite = get_kite()
+    instrument_token = get_token(symbol)
 
     to_date = datetime.now()
-    from_date = to_date - timedelta(days=days)
+    from_date = to_date - timedelta(days=20)
 
-    candles = kite.historical_data(token, from_date, to_date, interval)
-    df = pd.DataFrame(candles)
+    data = kite.historical_data(
+        instrument_token,
+        from_date,
+        to_date,
+        "day"
+    )
+
+    df = pd.DataFrame(data)
+
+    # Save in cache
+    CACHE[symbol] = (df, now)
+
     return df
